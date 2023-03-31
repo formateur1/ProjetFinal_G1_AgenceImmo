bda0df944d0267e55ac7204953af7e266ad16890
Mariambmb <mariambamba92@live.com>
2023-03-31 15:16:23 +0200

    test client

--- src/main/java/com/inti/repository/IClientRepository.java
+++ src/main/java/com/inti/repository/IClientRepository.java
@@ -1,5 +1,7 @@
 package com.inti.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.inti.model.Client;
 @Repository
 public interface IClientRepository extends JpaRepository<Client, Integer>{
 
+	List<Client> findByNom(String nom);
 }
