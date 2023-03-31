061cd8b6e13fb8793efb48b8aeb8a54808cca3c3
lucieargaud <lucieargaud@gmail.com>
2023-03-31 14:21:06 +0200

    App module

--- ProjetFinal_G1/src/app/app.module.ts
+++ ProjetFinal_G1/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormClientComponent } from './form-client/form-client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormClientComponent
   ],
   imports: [
     BrowserModule,
