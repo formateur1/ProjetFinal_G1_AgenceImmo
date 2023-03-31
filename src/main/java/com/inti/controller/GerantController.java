6e5d1d9c4cb316277eeb9654aec5e092a26f7cfc
CharleneSanchez <sanchez.charlene@outlook.fr>
2023-03-31 10:34:48 +0200

    UpdateOffre

--- src/main/java/com/inti/controller/GerantController.java
+++ src/main/java/com/inti/controller/GerantController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,6 +57,11 @@ public class GerantController {
 	public void deleteOffre(@PathVariable("id") int id)
 	{
 		ior.deleteById(id);
+	}
+	@PutMapping("updateOffre/{id}")
+	public void updateOffre(@RequestBody Offre o)
+	{
+		ior.save(o);
 	}
 	
 	
