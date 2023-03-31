3bd64799d0af102cb2922ee46c84b99c23b4bf68
CharleneSanchez <sanchez.charlene@outlook.fr>
2023-03-31 10:48:06 +0200

    Gerant lien front-back, maj repository gerant

--- src/main/java/com/inti/controller/GerantController.java
+++ src/main/java/com/inti/controller/GerantController.java
@@ -3,6 +3,7 @@ package com.inti.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.inti.repository.IOffreRepository;
 
 @RestController
 @RequestMapping("gerant")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class GerantController {
 	@Autowired
 	IClientRepository icr;
