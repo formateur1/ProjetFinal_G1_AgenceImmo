80b90ea147f5babe51db5f24b4ca7a401fd07e73
tmichielin <theodore.michielin@hotmail.fr>
2023-03-31 12:22:59 +0200

    Ajout de PostMapping et GetMapping sur OffreController

--- src/main/java/com/inti/controller/OffreController.java
+++ src/main/java/com/inti/controller/OffreController.java
@@ -3,18 +3,23 @@ package com.inti.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.model.Offre;
 import com.inti.repository.IOffreRepository;
 
 @RestController
+@RequestMapping("/offres")
 public class OffreController {
 	
 	@Autowired
 	IOffreRepository ior;
 	
+	@PostMapping("/{offreId}/ajouterNote/{note}")
 	public void ajouterNote(@PathVariable int offreId, @PathVariable double note)
 	{
 		Optional<Offre> ofr = ior.findById(offreId);
@@ -23,6 +28,7 @@ public class OffreController {
 		ior.save(offre);
 	}
 	
+	@GetMapping("/{offreId}/moyenneNotes")
 	public double MoyenneNotes(@PathVariable int offreId)
 	{
 		Optional<Offre> ofr = ior.findById(offreId);
