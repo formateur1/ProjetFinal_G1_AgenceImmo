23abe812fcca28997e646d8408553346e1a9d0b8
lucieargaud <lucieargaud@gmail.com>
2023-04-04 12:38:15 +0200

    Controllers admin et gerant

--- src/main/java/com/inti/controller/AdministrateurController.java
+++ src/main/java/com/inti/controller/AdministrateurController.java
@@ -3,6 +3,7 @@ package com.inti.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.inti.repository.IGerantRepository;
 
 @RestController
 @RequestMapping("admin")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:4201"})
 public class AdministrateurController {
 
 	@Autowired
--- src/main/java/com/inti/controller/GerantController.java
+++ src/main/java/com/inti/controller/GerantController.java
@@ -3,6 +3,7 @@ package com.inti.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,17 +14,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.model.Client;
+import com.inti.model.Gerant;
 import com.inti.model.Offre;
 import com.inti.repository.IClientRepository;
+import com.inti.repository.IGerantRepository;
 import com.inti.repository.IOffreRepository;
 
 @RestController
 @RequestMapping("gerant")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:4201"})
 public class GerantController {
 	@Autowired
 	IClientRepository icr;
 	@Autowired
 	IOffreRepository ior;
+	@Autowired
+	IGerantRepository igr; 
 
 	@GetMapping("listeClients")
 	public List<Client> listeClients() {
@@ -65,5 +71,16 @@ public class GerantController {
 	}
 	
 	
-	
+	@PostMapping("ajouterGerant")
+	public Gerant inscriptionGerant(@RequestBody Gerant g)
+	{
+			
+		return igr.save(g);
+	}
+		
+	@GetMapping("getGerants")
+	public List<Gerant> getGerants()
+	{
+		return igr.findAll();
+	}
 }
