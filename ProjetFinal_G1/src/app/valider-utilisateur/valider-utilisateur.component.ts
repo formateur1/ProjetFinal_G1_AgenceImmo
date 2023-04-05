4699e6ec786063d45f2323171386ed0b443b4432
lucieargaud <lucieargaud@gmail.com>
2023-04-05 16:39:10 +0200

    location pour le refresh des pages + index

--- ProjetFinal_G1/src/app/valider-utilisateur/valider-utilisateur.component.ts
+++ ProjetFinal_G1/src/app/valider-utilisateur/valider-utilisateur.component.ts
@@ -7,6 +7,7 @@ import { GerantServiceService } from '../service/gerant-service.service';
 import { AdminService } from '../service/admin.service';
 import { Gerant } from '../model/gerant.model';
 import { Administrateur } from '../model/administrateur.model';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-valider-utilisateur',
@@ -30,6 +31,7 @@ supprimerAdmin(id:string)
 {
   this.as.delete(parseInt(id)).subscribe();
   this.router.navigate(['validerUtilisateur'])
+  location.reload()
 }
 
 validerAdmin(a:Administrateur, id:string)
@@ -37,11 +39,13 @@ validerAdmin(a:Administrateur, id:string)
   this.as.delete(parseInt(id)).subscribe(); 
   this.as.validerAdmin(a).subscribe();
   this.router.navigate(['validerUtilisateur'])
+  location.reload()
 }
 supprimerGerant(id:string)
 {
   this.as.deleteGerant(parseInt(id)).subscribe();
   this.router.navigate(['validerUtilisateur'])
+  location.reload()
 }
 
 validerGerant(g:Gerant, id:string)
@@ -49,11 +53,13 @@ validerGerant(g:Gerant, id:string)
   this.as.deleteGerant(parseInt(id)).subscribe(); 
   this.as.validerGerant(g).subscribe();
   this.router.navigate(['validerUtilisateur'])
+  location.reload()
 }
 supprimerClient(id:string)
 {
   this.as.deleteClient(parseInt(id)).subscribe();
   this.router.navigate(['validerUtilisateur'])
+  location.reload()
 }
 
 validerClient(c:Client, id:string)
@@ -61,5 +67,6 @@ validerClient(c:Client, id:string)
   this.as.deleteClient(parseInt(id)).subscribe(); 
   this.as.validerClient(c).subscribe();
   this.router.navigate(['validerUtilisateur'])
+  location.reload()
 }
 }
