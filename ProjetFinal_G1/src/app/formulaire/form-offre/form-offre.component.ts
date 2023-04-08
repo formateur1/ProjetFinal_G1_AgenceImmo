25c010a59656a2d7f8058d560d1293033561462d
CharleneSanchez <sanchez.charlene@outlook.fr>
2023-04-08 16:58:45 +0200

    corrections erreurs duent au merge

--- ProjetFinal_G1/src/app/formulaire/form-offre/form-offre.component.ts
+++ ProjetFinal_G1/src/app/formulaire/form-offre/form-offre.component.ts
@@ -11,73 +11,71 @@ import { OffreService } from 'src/app/service/offre.service';
   styleUrls: ['./form-offre.component.css']
 })
 export class FormOffreComponent implements OnInit {
- 
-  constructor(private fb:FormBuilder, private http:HttpClient, private os:OffreService){}
+
+  constructor(private fb: FormBuilder, private http: HttpClient, private os: OffreService) { }
 
   ngOnInit(): void {
-    this.offreForm=this.fb.group({
-      type_bien:"",
-      adresse:"",
-      ville:"",
-      prix:0,
-      surface:0,
-      nb_piece:0,
-      meuble:false,
-      achat:false,
-      exterieur:false,
-      croquis:""
-      
+    this.offreForm = this.fb.group({
+      type_bien: "",
+      adresse: "",
+      ville: "",
+      prix: 0,
+      surface: 0,
+      nb_piece: 0,
+      meuble: false,
+      achat: false,
+      exterieur: false,
+      croquis: ""
+
     })
   }
-   
-     type_bien:string="";
-     adresse:string="";
-     ville:string="";
-     prix!:number;
-     surface!:number;
-     nb_piece!:number;
-     meuble!:boolean;
-     achat!:boolean;
-     exterieur!:boolean;
-     croquis:string="";
 
-     savedOffre!:Offre;
-     offreForm!:FormGroup;
-     savedMeuble!:boolean;
-     savedExt!:boolean;
-     savedAchat!:boolean;
-     
+  type_bien: string = "";
+  adresse: string = "";
+  ville: string = "";
+  prix!: number;
+  surface!: number;
+  nb_piece!: number;
+  meuble!: boolean;
+  achat!: boolean;
+  exterieur!: boolean;
+  croquis: string = "";
 
-
-  addOffre()
-  {
-
-    if( this.offreForm.value.meuble == "true"){
-
-      this.savedMeuble=true;
-    } else{ this.savedMeuble=false}
-
-    if( this.offreForm.value.exterieur == "true"){
-      this.savedExt=true;
-    }else{ this.savedExt=false}
-    if( this.offreForm.value.achat == "true"){
-      this.savedAchat=true;
-    } else{ this.savedAchat=false}
+  savedOffre!: Offre;
+  offreForm!: FormGroup;
+  savedMeuble!: boolean;
+  savedExt!: boolean;
+  savedAchat!: boolean;
 
 
-    this.savedOffre= new Offre
-    (
-      this.offreForm.value.type_bien,this.offreForm.value.adresse,this.offreForm.value.ville,
-      this.offreForm.value.prix,this.offreForm.value.surface,this.offreForm.value.nb_piece,
-      this.savedMeuble, this.savedAchat,this.savedExt,this.offreForm.value.croquis
-    )
 
-   if(this.savedOffre!=null) {*/
+  addOffre() {
+
+    if (this.offreForm.value.meuble == "true") {
+
+      this.savedMeuble = true;
+    } else { this.savedMeuble = false }
+
+    if (this.offreForm.value.exterieur == "true") {
+      this.savedExt = true;
+    } else { this.savedExt = false }
+    if (this.offreForm.value.achat == "true") {
+      this.savedAchat = true;
+    } else { this.savedAchat = false }
+
+
+    this.savedOffre = new Offre
+      (
+        this.offreForm.value.type_bien, this.offreForm.value.adresse, this.offreForm.value.ville,
+        this.offreForm.value.prix, this.offreForm.value.surface, this.offreForm.value.nb_piece,
+        this.savedMeuble, this.savedAchat, this.savedExt, this.offreForm.value.croquis
+      )
+
+    if (this.savedOffre != null) {
       this.os.saveOffre(this.offreForm.value).subscribe();
       console.log(this.offreForm.value);
-   // } else { "formulaire correct : " + this.savedOffre.adresse + " "+this.savedOffre.prix +"€ pour "+ this.savedOffre.surface+"m2."
-   //   alert("erreur dans le formulaire, offre incorrecte ")
-   // }
-
+    } else {
+      alert("erreur dans le formulaire, offre incorrecte ")
+    }
   }
 }
