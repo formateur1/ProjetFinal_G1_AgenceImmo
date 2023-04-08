667a4da493a6fb064c444c5ec939443c46882bcc
CharleneSanchez <sanchez.charlene@outlook.fr>
2023-04-09 01:31:21 +0200

    message echec connexion si entre un mauvais id dans compte

--- ProjetFinal_G1/src/app/compte/compte.component.ts
+++ ProjetFinal_G1/src/app/compte/compte.component.ts
@@ -7,22 +7,22 @@ import { AdminService } from '../service/admin.service';
   templateUrl: './compte.component.html',
   styleUrls: ['./compte.component.css']
 })
-export class CompteComponent implements OnInit{
+export class CompteComponent implements OnInit {
 
-  id!:number
-  nom!:string
-  prenom!:string
-  compteForm$!:FormGroup
-  role!:string
-  compteFormComplet!:FormGroup
+  id!: number
+  nom!: string
+  prenom!: string
+  compteForm$!: FormGroup
+  role!: string
+  compteFormComplet!: FormGroup
 
-  constructor(private as:AdminService, private fb:FormBuilder){}
+  constructor(private as: AdminService, private fb: FormBuilder) { }
 
 
   ngOnInit(): void {
     this.compteForm$ = this.fb.group({
-      id:[null, Validators.required],
-      role:[null]
+      id: [null, Validators.required],
+      role: [null]
     })
 
     if (sessionStorage.getItem('reload') == 'true') {
@@ -31,61 +31,64 @@ export class CompteComponent implements OnInit{
     }
   }
 
-  saveData(){
-    if(this.compteForm$.value.role == 'client' ) {
-    this.as.getClientById(this.compteForm$.value.id).subscribe(data => {
-      this.compteFormComplet = this.fb.group({
-        id:[data.id],
-        nom:[data.nom],
-        prenom:[data.prenom]
+  saveData() {
+    if (this.compteForm$.value.role == 'client') {
+      this.as.getClientById(this.compteForm$.value.id).subscribe(data => {
+        this.compteFormComplet = this.fb.group({
+          id: [data.id],
+          nom: [data.nom],
+          prenom: [data.prenom]
+        })
+
+        sessionStorage.setItem('id', this.id.toString())
+        sessionStorage.setItem('nom', this.compteFormComplet.value.nom)
+        sessionStorage.setItem('prenom', this.compteFormComplet.value.prenom)
+        sessionStorage.setItem('connecte', 'ok')
+        sessionStorage.setItem('role', 'client')
+
+        alert("Vous êtes connecté (Client)")
+        location.reload()
+      })
+    } if (this.compteForm$.value.role == 'gerant') {
+      this.as.getGerantById(this.compteForm$.value.id).subscribe(data => {
+        this.compteFormComplet = this.fb.group({
+          id: [data.id],
+          nom: [data.nom],
+          prenom: [data.prenom]
+        })
+        sessionStorage.setItem('id', this.id.toString())
+        sessionStorage.setItem('nom', this.compteFormComplet.value.nom)
+        sessionStorage.setItem('prenom', this.compteFormComplet.value.prenom)
+        sessionStorage.setItem('connecte', 'ok')
+        sessionStorage.setItem('role', 'gerant')
+
+        alert("Vous êtes connecté (Gérant)")
+        location.reload()
+      })
+    } if (this.compteForm$.value.role == 'admin') {
+      this.as.getAdminById(this.compteForm$.value.id).subscribe(data => {
+        this.compteFormComplet = this.fb.group({
+          id: [data.id],
+          nom: [data.nom],
+          prenom: [data.prenom]
+        })
+        sessionStorage.setItem('id', this.id.toString())
+        sessionStorage.setItem('nom', this.compteFormComplet.value.nom)
+        sessionStorage.setItem('prenom', this.compteFormComplet.value.prenom)
+        sessionStorage.setItem('connecte', 'ok')
+        sessionStorage.setItem('role', 'admin')
+
+        alert("Vous êtes connecté (Admin)")
+        location.reload()
       })
 
-      sessionStorage.setItem('id', this.id.toString())
-      sessionStorage.setItem('nom', this.compteFormComplet.value.nom)
-      sessionStorage.setItem('prenom', this.compteFormComplet.value.prenom)
-      sessionStorage.setItem('connecte', 'ok')
-      sessionStorage.setItem('role', 'client')
-
-      alert("Vous êtes connecté (Client)")
-      location.reload()
-    })
-  } if(this.compteForm$.value.role == 'gerant' ) {
-    this.as.getGerantById(this.compteForm$.value.id).subscribe(data => {
-      this.compteFormComplet = this.fb.group({
-        id:[data.id],
-        nom:[data.nom],
-        prenom:[data.prenom]
-      })
-      sessionStorage.setItem('id', this.id.toString())
-      sessionStorage.setItem('nom', this.compteFormComplet.value.nom)
-      sessionStorage.setItem('prenom', this.compteFormComplet.value.prenom)
-      sessionStorage.setItem('connecte', 'ok')
-      sessionStorage.setItem('role', 'gerant')
-    
-      alert("Vous êtes connecté (Gérant)")
-      location.reload()
-    })
-  } if (this.compteForm$.value.role == 'admin' ) {
-    this.as.getAdminById(this.compteForm$.value.id).subscribe(data => {
-      this.compteFormComplet = this.fb.group({
-        id:[data.id],
-        nom:[data.nom],
-        prenom:[data.prenom]
-      })
-      sessionStorage.setItem('id', this.id.toString())
-      sessionStorage.setItem('nom', this.compteFormComplet.value.nom)
-      sessionStorage.setItem('prenom', this.compteFormComplet.value.prenom)
-      sessionStorage.setItem('connecte', 'ok')
-      sessionStorage.setItem('role', 'admin')
-    
-      alert("Vous êtes connecté (Admin)")
-      location.reload()
-  })
-
-}}
+    } else {
+      alert("ECHEC CONNEXION : id "+this.compteForm$.value.id);
+    }
+  }
 
 
-deco(){
+  deco() {
     sessionStorage.clear()
     alert("Vous êtes déconnecté")
     location.reload()
